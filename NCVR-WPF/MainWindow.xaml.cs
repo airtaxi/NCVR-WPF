a64d04689c3b5c5b83e2394a11e6b1d4085c83b5
Howon Lee <kck4156@kagamine-rin.com>
2021-08-12 00:12:21 +0900

    using 정리

--- NCVR-WPF/MainWindow.xaml.cs
+++ NCVR-WPF/MainWindow.xaml.cs
@@ -1,21 +1,7 @@
-﻿using ControlzEx.Standard;
+﻿using System.Windows;
 using MahApps.Metro.Controls.Dialogs;
 using Newtonsoft.Json.Linq;
 using RestSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NCVR_WPF
 {
