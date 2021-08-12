8cbdb08398d5d78bc26e89b4e3abed34ed6d55d2
Howon Lee <kck4156@kagamine-rin.com>
2021-08-12 15:15:50 +0900

    URL 로직 오류 수정

--- NCVR-WPF/MainWindow.xaml.cs
+++ NCVR-WPF/MainWindow.xaml.cs
@@ -102,12 +102,13 @@ namespace NCVR_WPF
                 response = await client.ExecuteAsync(request);
                 responseJson = JObject.Parse(response.Content);
                 responseCode = responseJson.Value<string>("rsltCd");
-                var reqId = responseJson.Value<string>("reqId");
-                string url = "https://ncvr2.kdca.go.kr/svc/waiting?reqId=" + reqId;
                 if (responseCode == "CONFLICT")
                     await this.ShowMessageAsync("오류", "동일인은 한번 접속된 후 10분 이후에 접속이 가능합니다.");
                 else if (responseCode == "B000")
                 {
+                    var reqId = responseJson.Value<string>("reqId");
+                    string url = "https://ncvr2.kdca.go.kr/svc/waiting?reqId=" + reqId;
+
                     var width = Width;
                     Width = 880;
                     if ((await this.ShowMessageAsync("안내", $"본인인증이 완료되었습니다.\n" + "접속 후 페이지 오류 또는 실수로 탭을 닫은 경우 https://ncvr2.kdca.go.kr/svc/waiting?reqId=" + reqId + "로 접속하시기 바랍니다.\n본 주소로 접속 시 10분 동안 재인증 대기를 하실 필요가 없습니다.\n주소를 클립보드에 복사하시겠습니까?", MessageDialogStyle.AffirmativeAndNegative)) == MessageDialogResult.Affirmative)
@@ -118,11 +119,10 @@ namespace NCVR_WPF
                     }
                     else
                         await this.ShowMessageAsync("안내", "본 메시지창을 닫으면 신청 웹 페이지가 열립니다.");
+                    System.Diagnostics.Process.Start(url);
                 }
                 else
                     await this.ShowMessageAsync("오류", $"{responseJson.Value<string>("rsltMsg")}");
-                if (!string.IsNullOrEmpty(url))
-                    System.Diagnostics.Process.Start(url);
             }
             else
             {
